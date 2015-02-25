c9283bae604c686410797792f8dfb91c009f316e
Vanessa Novak <vnovak@hetcak.nl>
2015-02-25 13:41:25 +0100

    arptables split into smaller modules, arptables data acceptatie iam

---
+++ manifests/init.pp
@@ -0,0 +1,25 @@
+class rclocal {
+  $rclocal_file = '/etc/rc.d/rc.local'
+
+  concat { $rclocal_file:
+    owner => 'root',
+    group => 'root',
+    mode  => '0755'
+  }
+
+  concat::fragment{ 'rclocal_header':
+    target  => $rclocal_file,
+    content => "#!/bin/sh\n# Managed by puppet - do not modify\ntouch /var/lock/subsys/local\n",  #newlines \n and tabs \t MUST be specified in DOUBLE-quoted strings
+    order   => '01'
+  }
+}
+
+# used by other modules to register themselves in the rclocal
+define rclocal::register($content="", $order='10') {
+
+  concat::fragment{ "rclocal_fragment_${name}":
+    target  => $rclocal::rclocal_file,
+    content => $content,
+    order   => $order,
+  }
+}
